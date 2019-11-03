4ccbd5395175b0637769437b54f8f8a9d398c320
Soumyadeep21 <soumo.sinha21@gmail.com>
2019-11-03 23:32:39 +0530

    core team model updated

--- lib/models/core_team.dart
+++ lib/models/core_team.dart
@@ -5,21 +5,41 @@ class CoreTeam {
   String name;
   String desciption;
   String imgUrl;
+  String twitter;
+  String linkedin;
+  String github;
+  String facebook;
 
-  CoreTeam({this.documentId, this.name, this.desciption, this.imgUrl});
+  CoreTeam({
+    this.documentId,
+    this.name,
+    this.desciption,
+    this.imgUrl,
+    this.facebook,
+    this.github,
+    this.linkedin,
+    this.twitter,
+  });
 
   factory CoreTeam.fromFireStore(DocumentSnapshot doc) {
     Map data = doc.data;
     return CoreTeam(
-      documentId: doc.documentID,
-      name: data['name'],
-      desciption: data['description'],
-      imgUrl: data['imgUrl'],
-    );
+        documentId: doc.documentID,
+        name: data['name'],
+        desciption: data['description'],
+        imgUrl: data['imgUrl'],
+        facebook: data['facebook'],
+        twitter: data['twitter'],
+        github: data['github'],
+        linkedin: data['linkedin']);
   }
   toJson() => {
         'name': name,
         'description': desciption,
         'imgUrl': imgUrl,
+        'facebook': facebook,
+        'twitter': twitter,
+        'github': github,
+        'linkedin': linkedin,
       };
 }
